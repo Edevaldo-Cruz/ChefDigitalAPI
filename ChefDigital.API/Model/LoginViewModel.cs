639de174b7e8da6b97894e458b88b17931fe8339
Edevaldo-Cruz <edevaldo_cruz@hotmail.com>
2023-09-18 20:06:12 -0300

    feat(API): Iniciando trabalhos na controller

---
+++ ChefDigital.API/Model/LoginViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ChefDigital.API.Model
+{
+    public class Login
+    {
+        public string email { get; set; }
+        public string senha { get; set; }
+        public string cpf { get; set; }
+    }
+}
--- ChefDigital.API/Program.cs
+++ ChefDigital.API/Program.cs
@@ -1,25 +1,107 @@
+using AutoMapper;
+using ChefDigital.Domain.Interfaces.Generics;
+using ChefDigital.Entities.Entities;
+using ChefDigital.Infra.Configuration;
+using ChefDigital.Infra.Repository.Generics;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using ChefDigital.API.Token;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// ConfigServices
+builder.Services.AddDbContext<ContextBase>(options =>
+              options.UseSqlServer(
+                  builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ContextBase>();
+builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+
+
+// INTERFACE E REPOSITORIO
+builder.Services.AddSingleton(typeof(IGeneric<>), typeof(RepositoryGenerics<>));
+//builder.Services.AddSingleton<IMessage, RepositoryMessage>();
+
+// SERVIÇO DOMINIO
+//builder.Services.AddSingleton<IServiceMessage, ServiceMessage>();
+
+// JWT
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+      .AddJwtBearer(option =>
+      {
+          option.TokenValidationParameters = new TokenValidationParameters
+          {
+              ValidateIssuer = false,
+              ValidateAudience = false,
+              ValidateLifetime = true,
+              ValidateIssuerSigningKey = true,
+
+              ValidIssuer = "Teste.Securiry.Bearer",
+              ValidAudience = "Teste.Securiry.Bearer",
+              IssuerSigningKey = JwtSecurityKey.Create("Secret_Key-12345678")
+          };
+
+          option.Events = new JwtBearerEvents
+          {
+              OnAuthenticationFailed = context =>
+              {
+                  Console.WriteLine("OnAuthenticationFailed: " + context.Exception.Message);
+                  return Task.CompletedTask;
+              },
+              OnTokenValidated = context =>
+              {
+                  Console.WriteLine("OnTokenValidated: " + context.SecurityToken);
+                  return Task.CompletedTask;
+              }
+          };
+      });
+
+var config = new AutoMapper.MapperConfiguration(cfg =>
+{
+    //cfg.CreateMap<MessageViewModel, Message>();
+    //cfg.CreateMap<Message, MessageViewModel>();
+});
+
+IMapper mapper = config.CreateMapper();
+builder.Services.AddSingleton(mapper);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
+//var urlDev = "https://dominiodocliente.com.br";
+//var urlHML = "https://dominiodocliente2.com.br";
+//var urlPROD = "https://dominiodocliente3.com.br";
+
+//app.UseCors(b => b.WithOrigins(urlDev, urlHML, urlPROD));
+
+var devClient = "http://localhost:4200";
+app.UseCors(x => x
+.AllowAnyOrigin()
+.AllowAnyMethod()
+.AllowAnyHeader().WithOrigins(devClient));
+
 app.UseHttpsRedirection();
-app.UseStaticFiles();
 
-app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.MapControllers();
+app.UseSwaggerUI();
 
 app.Run();
