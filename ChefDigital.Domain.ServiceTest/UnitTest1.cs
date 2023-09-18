0fdd58cfc76bb96cb6c60b5f6d6d6db7c42b6fa0
Edevaldo-Cruz <edevaldo_cruz@hotmail.com>
2023-09-18 18:33:38 -0300

    Adicionar arquivos de projeto.

---
+++ ChefDigital.Domain.ServiceTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace ChefDigital.Domain.ServiceTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ ChefDigital.Teste.DomainServiceTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ChefDigital.Teste.DomainServiceTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
