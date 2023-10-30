3d80890d8e32c130076a2767144018c2733366de
Edevaldo-Cruz <edevaldo_cruz@hotmail.com>
2023-10-30 19:03:39 -0300

    feat(ClientListServiceTest): Implementando teste unitario ListAsync_MustReturnACustomerList_WhenCalled

---
+++ ChefDigital.Domain.Test/Client/ClientListServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using Castle.Core.Resource;
+using ChefDigital.Domain.Interfaces;
+using ChefDigital.Domain.Service.Client;
+using Moq;
+using System.Linq.Expressions;
+
+namespace ChefDigital.Domain.Service.Test.Client
+{
+    public class ClientListServiceTest
+    {
+        [Fact]
+        [Trait("Description", "Verifica se o método ListAsync retorna uma lista de clientes")]
+        public async Task ListAsync_MustReturnACustomerList_WhenCalled()
+        {
+            //Arrange
+            var clientRepository = new Mock<IClientRepository>();
+
+            clientRepository.Setup(repo => repo.ClientListFilter(It.IsAny<Expression<Func<Entities.Entities.Client, bool>>>()))
+                .Returns(Task.FromResult(new List<Entities.DTO.Client.ClientListDTO>()));
+
+
+            var clientListService = new ClientListService(clientRepository.Object);
+
+            //Act
+            var result = await clientListService.ListAsync();
+
+            //Assert
+            Assert.NotNull(result);
+        }
+    }
+}
