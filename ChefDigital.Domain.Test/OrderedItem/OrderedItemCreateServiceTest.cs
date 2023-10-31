9951563e62627b951875788e251a7c7c8766f3d9
Edevaldo-Cruz <edevaldo_cruz@hotmail.com>
2023-10-31 19:48:32 -0300

    feat(OrderedItemCreateServiceTest): Implementando teste unitario OrderedItem_MustReturnTrueWhenCalled.

--- ChefDigital.Domain.Test/OrderedItem/OrderedItemCreateServiceTest.cs
+++ ChefDigital.Domain.Test/OrderedItem/OrderedItemCreateServiceTest.cs
@@ -1,4 +1,5 @@
 ﻿using ChefDigital.Domain.Interfaces;
+using ChefDigital.Domain.Service.OrderedItem;
 using Moq;
 using System;
 using System.Collections.Generic;
@@ -10,12 +11,32 @@ namespace ChefDigital.Domain.Service.Test.OrderedItem
 {
     public class OrderedItemCreateServiceTest
     {
-        //[Fact]
-        //public async Task OrderedItemCreateServiceTest() 
-        //{
-        //    var orderedItemRepository = new Mock<IOrderedItemRepository>();
-        //    orderedItemRepository.Setup(repo => repo.)
+        [Fact]
+        public async Task OrderedItem_MustReturnTrueWhenCalled()
+        {
+            //Arrange
+            var orderedItem = new Entities.Entities.OrderedItem
+            {
+                OrderId = Guid.NewGuid(),
+                Item = "Rice",
+                UnitValue = 10,
+                ItemQuantity = 2,
+            };
+            var orderedItemRepository = new Mock<IOrderedItemRepository>();
 
-        //}
+            orderedItemRepository.Setup(repo => repo
+                .Add(It.IsAny<Entities.Entities.OrderedItem>()))
+                .ReturnsAsync(orderedItem);
+
+            var orderedItemCreateService = new OrderedItemCreateService(orderedItemRepository.Object);
+
+            //Act
+            var result = await orderedItemCreateService.CreateAsync(orderedItem);
+
+            //Assert
+            Assert.True(result);
+        }
+
+       
     }
 }
