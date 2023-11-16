673f8248af2670210042660ee0d9559f2abf8b70
Edevaldo-Cruz <edevaldo_cruz@hotmail.com>
2023-11-16 20:02:18 -0300

    feat(OrderUpdateStatusServiceTest) Implementando testes unitarios.

--- ChefDigital.Domain.Test/Order/OrderUpdateStatusServiceTest.cs
+++ ChefDigital.Domain.Test/Order/OrderUpdateStatusServiceTest.cs
@@ -1,20 +1,24 @@
 ﻿using ChefDigital.Domain.Interfaces;
 using ChefDigital.Domain.Service.Order;
-using ChefDigital.Entities.Entities;
-using ChefDigital.Infra.Repository.Repositories;
 using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ChefDigital.Domain.Service.Test.Order
 {
-    public class OrderCancelServiceTest
+    public class OrderUpdateStatusServiceTest
     {
         [Fact]
-        [Trait("Description", "Teste unitario verifica se o pedido vai ser salvo com o status cancelado.")]
-        public async Task CancelOrderAsync_MustReturnOrderWithStatusCanceled_WhenCalled()
+        [Trait("Description", "Teste unitario verifica se o pedido vai ser salvo com o proximo status.")]
+        public async Task UpdateOrderStatusAsync_MustChangeStatus_WhenCalling()
         {
             //Arrange
-            Guid id = Guid.NewGuid();
             var orderRepository = new Mock<IOrderRepository>();
+            Guid id = Guid.NewGuid();
+
             var order = new Entities.Entities.Order
             {
                 Id = id,
@@ -35,24 +39,24 @@ namespace ChefDigital.Domain.Service.Test.Order
                 .GetEntityById(It.IsAny<Guid>()))
                 .ReturnsAsync(order);
 
-            var orderCancelService = new OrderCancelService(orderRepository.Object);
+            var orderUpdateStatusService = new OrderUpdateStatusService(orderRepository.Object);
 
             //Act
-            var result = await orderCancelService.CancelOrderAsync(id);
+            var result = await orderUpdateStatusService.UpdateOrderStatusAsync(id);
 
             //Assert
             Assert.NotNull(result);
-            Assert.Equal(result.Status, Entities.Enums.OrderStatusEnum.Canceled);
-
+            Assert.Equal(Entities.Enums.OrderStatusEnum.InPreparation, result.Status);
         }
 
         [Fact]
         [Trait("Description", "Teste unitario verifica se o pedido vai retorna a notificação 'Pedido não encontrado.'")]
-        public async Task CancelOrderAsync_MustReturnRequestNotFound_WhenCalled()
+        public async Task UpdateOrderStatusAsync_MustReturnRequestNotFound_WhenCalled()
         {
             //Arrange
-            Guid id = Guid.NewGuid();
             var orderRepository = new Mock<IOrderRepository>();
+            Guid id = Guid.NewGuid();
+
             var order = new Entities.Entities.Order
             {
                 Id = id,
@@ -69,23 +73,24 @@ namespace ChefDigital.Domain.Service.Test.Order
                 Discount = 2,
             };
 
-            var orderCancelService = new OrderCancelService(orderRepository.Object);
+            var orderUpdateStatusService = new OrderUpdateStatusService(orderRepository.Object);
 
             //Act
-            var result = await orderCancelService.CancelOrderAsync(id);
+            var result = await orderUpdateStatusService.UpdateOrderStatusAsync(id);
 
-            //Assert
+            //Assert           
             Assert.NotNull(result);
             Assert.True(result.Notitycoes.Any(n => n.Message == "Pedido não encontrado."));
         }
 
         [Fact]
         [Trait("Description", "Teste unitario verifica se o pedido vai retorna a notificação 'O pedido já se encontra cancelado.")]
-        public async Task CancelOrderAsync_MustReturnOrderIsAlreadyCanceled_WhenCalled()
+        public async Task UpdateOrderStatusAsync_MustReturnOrderIsAlreadyCanceled_WhenCalled()
         {
             //Arrange
-            Guid id = Guid.NewGuid();
             var orderRepository = new Mock<IOrderRepository>();
+            Guid id = Guid.NewGuid();
+
             var order = new Entities.Entities.Order
             {
                 Id = id,
@@ -107,10 +112,10 @@ namespace ChefDigital.Domain.Service.Test.Order
                 .GetEntityById(It.IsAny<Guid>()))
                 .ReturnsAsync(order);
 
-            var orderCancelService = new OrderCancelService(orderRepository.Object);
+            var orderUpdateStatusService = new OrderUpdateStatusService(orderRepository.Object);
 
             //Act
-            var result = await orderCancelService.CancelOrderAsync(id);
+            var result = await orderUpdateStatusService.UpdateOrderStatusAsync(id);
 
             //Assert
             Assert.NotNull(result);
@@ -119,12 +124,13 @@ namespace ChefDigital.Domain.Service.Test.Order
 
         [Fact]
         [Trait("Description", "Teste unitario verifica se o pedido vai retorna a notificação 'O pedido já foi entregue.")]
-        public async Task CancelOrderAsync_MustReturnOrderAlreadyDelivered_WhenCalled()
+        public async Task UpdateOrderStatusAsync_MustReturnOrderAlreadyDelivered_WhenCalled()
         {
             //Arrange
             int status = 5;
-            Guid id = Guid.NewGuid();
             var orderRepository = new Mock<IOrderRepository>();
+            Guid id = Guid.NewGuid();
+
             var order = new Entities.Entities.Order
             {
                 Id = id,
@@ -140,7 +146,6 @@ namespace ChefDigital.Domain.Service.Test.Order
                 },
                 Discount = 2,
             };
-
             for (int i = 0; i < status; i++)
             {
                 order.SetStatus();
@@ -150,14 +155,15 @@ namespace ChefDigital.Domain.Service.Test.Order
                 .GetEntityById(It.IsAny<Guid>()))
                 .ReturnsAsync(order);
 
-            var orderCancelService = new OrderCancelService(orderRepository.Object);
+            var orderUpdateStatusService = new OrderUpdateStatusService(orderRepository.Object);
 
             //Act
-            var result = await orderCancelService.CancelOrderAsync(id);
+            var result = await orderUpdateStatusService.UpdateOrderStatusAsync(id);
 
             //Assert
             Assert.NotNull(result);
             Assert.True(result.Notitycoes.Any(n => n.Message == "O pedido não pode ser cancelado pois já foi entregue."));
         }
+
     }
 }
